82abd392b22ec33a863480528107f1f15c6eb807
Reina Liew <notalk_0@hotmail.com>
2019-06-18 04:38:05 +0800

    fix bug

--- backend/middleware/file.js
+++ backend/middleware/file.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const isValid = MIME_TYPE_MAP[file.mimetype];
     let error = isValid ? null : new Error('Invalid mime type');
-    cb(error, 'backend/images');
+    cb(error, 'images');
   },
   filename: (req, file, cb) => {
     const name = file.originalname
