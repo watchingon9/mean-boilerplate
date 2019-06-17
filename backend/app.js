8eeed96394fac299b310f59524842ec2e813c197
Reina Liew <notalk_0@hotmail.com>
2019-06-18 04:38:51 +0800

    finalize

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/images', express.static(path.join('backend/images')));
+app.use('/images', express.static(path.join('images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
